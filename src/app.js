00c79cf5cc5c99179697466b5a7b381798192f71
Hal DeLage <haldelage@gmail.com>
2019-05-30 11:13:13 -0500

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-named-as-default-member */
+/* eslint-disable import/no-named-as-default */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+ReactDOM.render(<p>Boilerplate</p>, document.getElementById('app'));
